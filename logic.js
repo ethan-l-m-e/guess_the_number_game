6b2cb33c9d04c5301e6af564afdd57a736491349
Lim Ethan <69660935+ethan-l-m-e@users.noreply.github.com>
2024-01-11 23:37:02 +0800

    Add player turns
    After player guesses incorrectly 5 times, shows text that player loses. Display text showing the number of turns remaining.

--- logic.js
+++ logic.js
@@ -4,14 +4,17 @@ window.addEventListener("load", ready);
 // Main function of the game
 function ready() {
     let playerInput = document.getElementById("playerInput");
+    let playerTurnsText = document.getElementById("playerTurnsText");
     let responseText = document.getElementById("responseText");
     // Number to be guessed to win the game
     let myNumber = Math.floor(Math.random() * 9) + 1;
+    let turnsLeft = 5;
 
     playerInput.addEventListener("submit", (e) => {
         e.preventDefault();
 
         // Handle the player's input
+        decrementTurnsLeft();
         let guess = document.getElementById("guess");
         if (guess.value == myNumber) {
             playerWins();
@@ -27,6 +30,8 @@ function ready() {
     document.getElementById("resetButton").addEventListener("click", () => {
         myNumber = Math.floor(Math.random() * 9) + 1;
         responseText.innerHTML = "";
+        turnsLeft = 5;
+        playerTurnsText.innerHTML = `You have ${turnsLeft} turns left`;
     });
 
     /**
@@ -37,10 +42,29 @@ function ready() {
     }
 
     function playerGuessesWrong(guess) {
+        // Game is over if out of turns
+        if (turnsLeft == 0) {
+            playerLoses();
+            return
+        }
+
+        // Else feedback to the player
         if (guess < myNumber) {
             responseText.innerHTML = `Your guess, ${guess}, is too low`;
         } else {
             responseText.innerHTML = `Your guess, ${guess}, is too high`;
-        }
+        }       
+    }
+
+    function playerLoses() {
+        responseText.innerHTML = "You Lose!"
+    }
+
+    /**
+     * Functions for utility
+     */
+    function decrementTurnsLeft() {
+        turnsLeft--;
+        playerTurnsText.innerHTML = `You have ${turnsLeft} turns left`;
     }
 }
