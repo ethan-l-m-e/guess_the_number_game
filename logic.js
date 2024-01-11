f5cb72a0104e46b07c5782cfb8f7784395c632c3
Lim Ethan <69660935+ethan-l-m-e@users.noreply.github.com>
2024-01-11 11:34:17 +0800

    Add random number
    Selects a random number to be guessed. Clears the player's input after each guess.

--- logic.js
+++ logic.js
@@ -4,11 +4,18 @@ window.addEventListener("load", ready);
 // Main function of the game
 function ready() {
     let gameForm = document.getElementById("gameForm");
+
+    // Number to be guessed to win the game
+    let myNumber = Math.floor(Math.random() * 9) + 1;
+
     gameForm.addEventListener("submit", (e) => {
         e.preventDefault();
 
         // Handle the player's input
         let guess = document.getElementById("guess");
-        console.log(guess.value);
+        console.log(guess.value == myNumber);
+
+        // Clear the player's input
+        guess.value = "";
     })
 }
